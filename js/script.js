99bb74664c0d2002bd2d5657dd0850cc43a26b6d
Michael Wilson <ebonyprince2014@gmail.com>
2025-05-16 14:12:46 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ $(document).ready(function() {
 	});
 
 	var typed = new Typed(".typed", {
-		strings: ["Project Manager.", "Game Producer.", "Student."],
+		strings: ["Project Manager.", "Game Producer.", "Recent Graduate."],
 		typeSpeed: 70,
 		loop: true,
 		startDelay: 1000,
